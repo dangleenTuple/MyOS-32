cd0c3a3f54668f232e2e8d04e636ea9feaaff330
dangleenTuple <rachelromaine1999@gmail.com>
2024-06-13 19:36:53 +0000

    CHAPTER 3 - added comments

--- src/kernel/arch/x86/start.asm
+++ src/kernel/arch/x86/start.asm
@@ -1,10 +1,23 @@
+/*
+Global declarations and externs:
+
+_start and _kmain are declared as global symbols, meaning they can be accessed from other parts of the code.
+kmain, start_ctors, end_ctors, start_dtors, and end_dtors are declared as external symbols. This means they are defined elsewhere (probably in the kernel code) but referenced here.
+*/
 global _start, _kmain
 extern kmain, start_ctors, end_ctors, start_dtors, end_dtors
 
-    
+/*
+These lines define constants used for the Multiboot header.
+MULTIBOOT_HEADER_MAGIC is a specific value that identifies a Multiboot header.
+MULTIBOOT_HEADER_FLAGS defines some flags for the header (value 3 in this case).
+CHECKSUM is calculated by negating the sum of the magic number and flags, likely for basic verification.
+Entry Point (_start):
+*/
 %define MULTIBOOT_HEADER_MAGIC  0x1BADB002
 %define MULTIBOOT_HEADER_FLAGS	0x00000003
 %define CHECKSUM -(MULTIBOOT_HEADER_MAGIC + MULTIBOOT_HEADER_FLAGS)
+
 
 ;-- Entry point
 _start:
@@ -13,15 +26,26 @@ _start:
 ;-- Multiboot header --
 align 4
 
+/*
+uses dd (double word) assembly instruction to define 4-byte values.
+*/
 multiboot_header:
 dd MULTIBOOT_HEADER_MAGIC
 dd MULTIBOOT_HEADER_FLAGS
-dd CHECKSUM     
+dd CHECKSUM
 ;--/Multiboot header --
 
+//ebx is the general-purpose register we are going to be using
 start:
 	push ebx
-	 
+
+/*
+start_ctors points to the beginning of an array containing constructor functions.
+The loop uses a test instruction to compare ebx (pointing to current constructor) with end_ctors (end of the array).
+If ebx is less than end_ctors, the function pointed to by ebx is called (call [ebx]). This likely calls functions used to initialize parts of the kernel before kmain.
+ebx is then incremented by 4 to move to the next constructor function in the array.
+The loop continues until all constructors are called.
+*/
 static_ctors_loop:
    mov ebx, start_ctors
    jmp .test
@@ -31,9 +55,14 @@ static_ctors_loop:
 .test:
    cmp ebx, end_ctors
    jb .body
- 
+
    call kmain                      ; call kernel proper
- 
+
+
+/*
+After constructors, the kernel's main function, kmain, is called. This is where the actual kernel execution begins.
+A similar loop (static_dtors_loop) iterates through destructor functions (pointed to by start_dtors and ending at end_dtors) likely used for cleanup tasks before the system halts.
+*/
 static_dtors_loop:
    mov ebx, start_dtors
    jmp .test
@@ -43,6 +72,6 @@ static_dtors_loop:
 .test:
    cmp ebx, end_dtors
    jb .body
-	
+
 	cli ; stop interrupts
 	hlt ; halt the CPU
--- src/kernel/sdk/diskimage.sh
+++ src/kernel/sdk/diskimage.sh
@@ -1,30 +1,63 @@
 #!/bin/bash
 qemu-img create c.img 2M
 fdisk ./c.img  << EOF
+
+# Switch to Expert commands
 x
+
+# Change number of cylinders (1-1048576)
 c
 4
+
+# Change number of heads (1-256, default 16):
 h
 16
+
+# Change number of sectors/track (1-63, default 63)
 s
 63
+
+# Return to main menu
 r
+
+# Add a new partition
 n
+
+# Choose primary partition
 p
+
+# Choose partition number
 1
+
+# Choose first sector (1-4, default 1)
 1
+
+# Choose last sector, +cylinders or +size{K,M,G} (1-4, default 4)
 4
+
+# Toggle bootable flag
 a
+
+# Choose first partition for bootable flag
 1
+
+# Write table to disk and exit
 w
 EOF
 fdisk -l -u ./c.img
+
+# Using fdisk -l -u c.img, you get: 63 * 512 = 32256.
 losetup -o 32256 /dev/loop1 ./c.img
 
+# We create a EXT2 filesystem on this new device using:
 mke2fs /dev/loop1
+
+# We copy our files on a mounted disk:
 mount  /dev/loop1 /mnt/
 cp -R bootdisk/* /mnt/
 umount /mnt/
+
+# Install GRUB on the disk:
 grub --device-map=/dev/null << EOF
 device (hd0) ./c.img
 geometry (hd0) 4 16 63
@@ -33,4 +66,5 @@ setup (hd0)
 quit
 EOF
 
+# And finally we detach the loop device:
 losetup -d /dev/loop1
