6ea1b0368123902ee4ec73ab7426756e7b5d3169
dangleenTuple <rachelromaine1999@gmail.com>
2024-07-02 03:19:58 +0000

    core - fixed kernel.cc comments

--- src/kernel/core/kernel.cc
+++ src/kernel/core/kernel.cc
@@ -1,11 +1,10 @@
-
-#include <os.h>
-#include <boot.h>
+#include "os.h"
+#include "boot.h"
 
 
 static char* init_argv[2]={"init","-i"};
 
-/* charge les modules de depart */
+/* loads the startup modules */
 static void load_modules(multiboot_info* mbi){
 	
 	if (mbi->mods_count>0){
@@ -25,34 +24,35 @@ static void load_modules(multiboot_info* mbi){
 }
 
 
-/* le main du noyau */
+/* the main of the kernel */
 extern "C" void kmain(multiboot_info* mbi){
 	io.clear();
 	io.print("%s - %s -- %s %s \n",	KERNEL_NAME,
 									KERNEL_VERSION,
 									KERNEL_DATE,
 									KERNEL_TIME);
-	
+	//Initialize everything! First, archiecture in arch/x86
 	io.print("%s \n",KERNEL_LICENCE);
 	arch.init();
-	
+	//The virtual memory
 	io.print("Loading Virtual Memory Management \n");
 	vmm.init(mbi->high_mem);
-	
+	//File system management
 	io.print("Loading FileSystem Management \n");
 	fsm.init();
-	
+	//System calls
 	io.print("Loading syscalls interface \n");
 	syscall.init();
-	
+	//The system
 	io.print("Loading system \n");
 	sys.init();
-	
+	//The modules
 	io.print("Loading modules \n");
 	modm.init();
 	modm.initLink();
 
 
+	// register modules for accessing partitions and attempting to mount the first partition (hda0) onto the /boot directory formatted with an ext2 file system
 	modm.install("hda0","module.dospartition",0,"/dev/hda");
 	modm.install("hda1","module.dospartition",1,"/dev/hda");
 	modm.install("hda2","module.dospartition",2,"/dev/hda");
@@ -70,7 +70,7 @@ extern "C" void kmain(multiboot_info* mbi){
 	io.print("\n");
 	io.print("  ==== System is ready (%s - %s) ==== \n",KERNEL_DATE,KERNEL_TIME);
 	arch.enable_interrupt();
-	for (;;);
+	for (;;); //Infinite loop for kernel to wait for events and keep the system running
 	arch.shutdown();
 }
 
