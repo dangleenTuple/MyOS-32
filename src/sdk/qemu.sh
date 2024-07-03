e6d455c47df6f1c0b4db4c57f28e743b7cf710ae
dangleenTuple <rachelromaine1999@gmail.com>
2024-07-03 01:15:01 +0000

    Fixed qemu.sh issue!

--- src/sdk/qemu.sh
+++ src/sdk/qemu.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-qemu-i386 1024 -s -hda ./c.img  -curses -serial /dev/tty  -redir tcp:2323::23
+qemu-system-i386 -kernel ./bootdisk/kernel.elf -s -hda ./c.img  -curses -serial /dev/tty  -redir tcp:2323::23
